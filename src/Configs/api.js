73c97382c0131aad85c20f3ff73b6a7c2057acf4
bavani18 <bavaniabavani18@gmail.com>
2026-07-16 11:44:56 +0530

    qrpondy

--- src/Configs/api.js
+++ src/Configs/api.js
@@ -1,4 +1,4 @@
-
+ export const BASE_URL ="https://ucspondy-qr-production.up.railway.app"
  export const BASE_URL = "http://localhost:5000";
 
 // const BASE_URL = "https://smabackoffice-production-fc6a.up.railway.app";
